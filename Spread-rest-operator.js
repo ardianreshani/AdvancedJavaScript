a483c1d9d1770519cc426672b8b631a7d3ace63a
aRdiiaNaRdiiaN04 <youngcreative12@gmail.com>
2021-04-07 15:43:04 +0200

    Spread-rest-operator

---
+++ Spread-rest-operator.js
@@ -0,0 +1,29 @@
+// function sum(multiplier, ...numbers) {
+//   return multiplier * numbers.reduce((sum, numbers) => sum + numbers, 0);
+// }
+// console.log(sum(10, 2, 4)); // 60
+
+// const array = ["A", "R", "D", "I"];
+// const numberArray = ["1", "2", "3", "4"];
+
+// // const newArray = [...numberArray];
+// // console.log(numberArray); // (4) ["1", "2", "3", "4"]
+
+// // numberArray.push(5);
+// // console.log(numberArray); // (5) ["1", "2", "3", "4", 5]
+
+// // const [first, second, ...rest] = numberArray;
+// // console.log(first, second, rest); //
+
+// const person = {
+//   name: "Ardian",
+//   age: 23,
+//   favoriteFood: "Pica",
+//   address: {
+//     street: "Test 1234",
+//     city: "Radovlijca",
+//   },
+// };
+
+// const { name, age, ...rest } = person; // Ardian 23 {favoriteFood: "Pica", address: {…}}
+// console.log(name, age, rest);
